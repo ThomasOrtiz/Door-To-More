4dee0083b0b5377e16543cc65292da03e46ddac1
Matthew Rodgers (macbook) <mattmusic1117@gmail.com>
2016-07-22 01:50:52 -0400

    Petition Formatting

--- app/src/main/java/blackbaud/advocacymobileapp/PetitionActivity.java
+++ app/src/main/java/blackbaud/advocacymobileapp/PetitionActivity.java
@@ -2,6 +2,8 @@ package blackbaud.advocacymobileapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 public class PetitionActivity extends AppCompatActivity {
 
@@ -10,6 +12,9 @@ public class PetitionActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_petition);
+
+        TextView text = (TextView)findViewById(R.id.textView12);
+        text.setMovementMethod(new ScrollingMovementMethod());
 
     }
 }
