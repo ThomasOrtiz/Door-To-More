3c4c5ab6e8df6d417f418da30aaba3a82c327407
chris.brown <dcbrown06@gmail.com>
2016-07-21 20:41:30 -0400

    AddressValActivity and Problem Activity ui basics

--- app/src/main/java/blackbaud/advocacymobileapp/AddressValActivity.java
+++ app/src/main/java/blackbaud/advocacymobileapp/AddressValActivity.java
@@ -1,13 +1,20 @@
 package blackbaud.advocacymobileapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class AddressValActivity extends AppCompatActivity {
 
+    private String address;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_address_val);
+
+        Intent intent = getIntent();
+
+        address = intent.getStringExtra("address");
     }
 }
--- app/src/main/java/blackbaud/advocacymobileapp/ProblemActivity.java
+++ app/src/main/java/blackbaud/advocacymobileapp/ProblemActivity.java
@@ -1,7 +1,12 @@
 package blackbaud.advocacymobileapp;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class ProblemActivity extends AppCompatActivity {
 
@@ -9,5 +14,37 @@ public class ProblemActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_problem);
+    }
+
+    public void clickSave(View view) {
+        Intent intent = new Intent(this, AddProspectActivity.class);
+        EditText editText = (EditText) findViewById(R.id.problemEditText);
+        String message = editText.getText().toString();
+        //Save message
+        //Close constituent
+        startActivity(intent);
+    }
+
+    public void back(View view) {
+        Intent intent = new Intent(this, AddProspectActivity.class);
+        startActivity(intent);
+    }
+
+    public void openAlert(View view) {
+        new AlertDialog.Builder(this)
+                .setTitle("Alert Title")
+                .setMessage("Alert Message")
+                .setPositiveButton("test +", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        //stub
+                    }
+                })
+                .setNegativeButton("test -", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        //stub
+                    }
+                })
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
     }
 }
