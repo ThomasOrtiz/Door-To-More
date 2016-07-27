122c83494ec6625110f32adf84e862e70b55dda6
Thomas Ortiz <tdortiz@ncsu.edu>
2016-07-26 22:34:25 -0400

    Added a fun animation

--- app/src/main/java/blackbaud/advocacymobileapp/HomeActivity.java
+++ app/src/main/java/blackbaud/advocacymobileapp/HomeActivity.java
@@ -4,13 +4,23 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
+
+	private ImageButton ajar_door;
+	private ImageButton closed_door;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+		ajar_door = (ImageButton) findViewById(R.id.imageButton1);
+		ajar_door.setOnClickListener(this);
+
+		closed_door   = (ImageButton) findViewById(R.id.imageButton4);
+		closed_door.setOnClickListener(this);
     }
 
     public void onProsAddressClicked(View view) {
@@ -26,4 +36,19 @@ public class HomeActivity extends AppCompatActivity {
         startActivityForResult(nextScreen, 0);
         overridePendingTransition(0,0); //0 for no animation
     }
+
+	public void onClick(View v) {
+
+		switch (v.getId()) {
+
+			case R.id.imageButton1: //  clicked ajar
+				ajar_door.setVisibility(View.INVISIBLE);
+				closed_door.setVisibility(View.VISIBLE);
+				break;
+			case R.id.imageButton4: // clicked closed
+				ajar_door.setVisibility(View.VISIBLE);
+				closed_door.setVisibility(View.INVISIBLE);
+				break;
+		}
+	}
 }
